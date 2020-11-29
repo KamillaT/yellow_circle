facafec25e0a7d6558710165c4f6ac0079552e4f
KamillaT <62097619+KamillaT@users.noreply.github.com>
2020-11-29 19:27:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -38,10 +38,3 @@ if __name__ == '__main__':
     ex = MyWidget()
     ex.show()
     sys.exit(app.exec())
-
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = MyWidget()
-    ex.show()
-    sys.exit(app.exec_())
